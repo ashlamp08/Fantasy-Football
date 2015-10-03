267dfc8d196a5cdcc8d8c84bbbe223a5354aeaed
Ashok Dhungana <dhungana.ashok08@gmail.com>
2015-10-03 13:58:07 +0530

    transfer page updated

--- transfer.php
+++ transfer.php
@@ -1,35 +1,3 @@
-<DOCTYPE! html>
-<html>
-<style>
-	div{
-	overflow:hidden;
-    overflow-y: scroll;
-    height: 500px; // change this to desired height
-    display: inline-block;
-}
-    
-    #players{ position: absolute; padding: 10px; padding-right:15px; top: 100px; right: 500px; z-index: 99;}
-		
-</style>
-<body>
-<button id="GK_BUTTON" onclick="myfun('GK_TABLE')">GK</button>
-<button id="DF_BUTTON" onclick="myfun('DF_TABLE')">DF</button>	
-<button id="MF_BUTTON" onclick="myfun('MF_TABLE')">MF</button>
-<button id="FW_BUTTON" onclick="myfun('FW_TABLE')">FW</button>	
-
-<script>
-	function myfun(id)
-	{
-		document.getElementById("GK_TABLE").style.display = 'none'; 
-		document.getElementById("DF_TABLE").style.display = 'none'; 
-		document.getElementById("MF_TABLE").style.display = 'none'; 
-		document.getElementById("FW_TABLE").style.display = 'none'; 
-
-		document.getElementById(id).style.display = 'block';
-		document.getElementById(id).scrollTop = 0;
-	}
-</script>	
-	
 <?php
 	require 'connect.php';
 	
@@ -64,87 +32,91 @@
 				{array_push($FW,$player);	
 					$FW_size=$FW_size+1;	
 					}
-		}
-		//echo ($GK[1]['PLAYER_NAME']);
-		//echo '<br>';
-			
-		
-		
+		}		
 	}
-//	echo $GK_size;
+?>
+	
+<html>
+<style>
+	div{
+	overflow:hidden;
+    overflow-y: scroll;
+    height: 500px; // change this to desired height
+    display: inline-block;
+}
+    
+    #players{ position: absolute; padding: 10px; padding-right:15px; top: 100px; right: 500px; z-index: 99;}
+</style>
+
+<body>
+	
+<button id="GK_BUTTON" onclick="myfun('GK_TABLE')">GK</button>
+<button id="DF_BUTTON" onclick="myfun('DF_TABLE')">DF</button>	
+<button id="MF_BUTTON" onclick="myfun('MF_TABLE')">MF</button>
+<button id="FW_BUTTON" onclick="myfun('FW_TABLE')">FW</button>	
+
+<script>
+	function myfun(id)
+	{
+		document.getElementById("GK_TABLE").style.display = 'none'; 
+		document.getElementById("DF_TABLE").style.display = 'none'; 
+		document.getElementById("MF_TABLE").style.display = 'none'; 
+		document.getElementById("FW_TABLE").style.display = 'none'; 
+
+		document.getElementById(id).style.display = 'block';
+		document.getElementById(id).scrollTop = 0;
+	}
+</script>	
+	
+<?php	
+	///////////////////   GK TABLE   //////////////////////
 	
 	echo '<div id="GK_TABLE"><table >';
 	for($i=0; $i<$GK_size; $i++)
     {
-	
-   echo '<tr>';
-    echo '<td>'
-    .$GK[$i]['PLAYER_NAME']
-    .'</td>'
-    .'<td>'
-    .$GK[$i]['CLUB_ID']
-    .'</td>'
-    .'<td>'
-    .$GK[$i]['PRICE']
-    .'</td>'
-    ;        
-    echo '</tr>';
+		echo '<tr>';
+		echo '<td>' .$GK[$i]['PLAYER_NAME'] .'</td>'
+		.'<td>' .$GK[$i]['CLUB_ID'] .'</td>'
+		.'<td>'.$GK[$i]['PRICE'].'</td>';        
+		echo '</tr>';
     }
 	echo '</table></div>';
+	
+	///////////////////   DF TABLE   //////////////////////
 	
 	echo '<div style="display:none" id="DF_TABLE"><table >';
 	for($i=0; $i<$DF_size; $i++)
     {
-	
-   echo '<tr>';
-    echo '<td>'
-    .$DF[$i]['PLAYER_NAME']
-    .'</td>'
-    .'<td>'
-    .$DF[$i]['CLUB_ID']
-    .'</td>'
-    .'<td>'
-    .$DF[$i]['PRICE']
-    .'</td>'
-    ;        
+		echo '<tr>';
+		echo '<td>'.$DF[$i]['PLAYER_NAME'].'</td>'
+		.'<td>'.$DF[$i]['CLUB_ID'].'</td>'
+		.'<td>'.$DF[$i]['PRICE'].'</td>';        
     echo '</tr>';
     }
 	echo '</table></div>';
+	
+	///////////////////   MF TABLE   //////////////////////
 	
 	echo '<div style="display:none" id="MF_TABLE"><table >';
 	for($i=0; $i<$MF_size; $i++)
     {
-	
-   echo '<tr>';
-    echo '<td>'
-    .$MF[$i]['PLAYER_NAME']
-    .'</td>'
-    .'<td>'
-    .$MF[$i]['CLUB_ID']
-    .'</td>'
-    .'<td>'
-    .$MF[$i]['PRICE']
-    .'</td>'
-    ;        
-    echo '</tr>';
+		echo '<tr>';
+		echo '<td>'.$MF[$i]['PLAYER_NAME'].'</td>'
+		.'<td>'.$MF[$i]['CLUB_ID'].'</td>'
+		.'<td>'.$MF[$i]['PRICE'].'</td>';        
+		echo '</tr>';
     }
 	echo '</table></div>';
+	
+	///////////////////   FW TABLE   //////////////////////
 	
 	echo '<div style="display:none" id="FW_TABLE"><table >';
 	for($i=0; $i<$FW_size; $i++)
     {
-	
-   echo '<tr>';
-    echo '<td>'
-    .$FW[$i]['PLAYER_NAME']
-    .'</td>'
-    .'<td>'
-    .$FW[$i]['CLUB_ID']
-    .'</td>'
-    .'<td>'
-    .$FW[$i]['PRICE']
-    .'</td>'
-    ;        
+		echo '<tr>';
+		echo '<td>'.$FW[$i]['PLAYER_NAME'].'</td>'
+		.'<td>'.$FW[$i]['CLUB_ID'].'</td>'
+		.'<td>'.$FW[$i]['PRICE'].'</td>';        
     echo '</tr>';
     }
 	echo '</table></div>';
